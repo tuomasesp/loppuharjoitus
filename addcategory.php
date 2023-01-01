ad3b53964d283cad2fb368ca6f1d965a3563ae8a
tuomasesp <101732066+tuomasesp@users.noreply.github.com>
2023-01-02 01:18:46 +0200

    ...

--- addcategory.php
+++ addcategory.php
@@ -1,5 +1,25 @@
 <?php
 session_start();
-require('./inc/functions-php');
+require('./inc/functions.php');
 require('./headers.php');
 
+if(!isset($_SESSION['knimi'])){
+    http_response_code(403);
+    echo "Ei oikeuksia";
+    return;
+}
+
+$db = null;
+
+$input = json_decode(file_get_contents('php://input'));
+$name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+try {
+    $db = createSqliteConnection();
+    $sql = "insert into tuoteryhma (trnimi) values ('$name')";
+    executeInsert($db,$sql);
+    $data = array('id' => $db->lastInsertId(), 'trnimi' => $name);
+    print json_encode($data);    
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
--- addproduct.php
+++ addproduct.php
@@ -3,3 +3,14 @@ session_start();
 require('./inc/functions.php');
 require('./headers.php');
 
+if(!isset($_SESSION['knimi'])){
+    http_response_code(403);
+    echo "Ei oikeuksia";
+    return;
+}
+
+$db = null;
+
+$input = json_decode(file_get_contents('php://input'));
+$name = filter_var($input->name,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
